a02c016e3f9d2c4c46d30f22da3a70f3530d646b
MiiguelMM <miguelmelgarejosanchez@live.com>
2025-07-18 02:09:56 +0200

    Version colores y primera parte

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../components/common/Header'
 
-import Section1 from '../components/index/Section1'
+import Section1 from '../components/index/Section1/Section1'
+import Section2 from '../components/index/Section2/Section2'
 
 export default function Index() {
   return (
     <div>
       <Header></Header>
       <Section1></Section1>
+      
     </div>
   )
 }
